46f2921632520b45172d0531309a20c716e8e803
renyui-programming <163693699+renyui-programming@users.noreply.github.com>
2024-12-06 12:52:22 +0900

    課題007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>課題007</title>
+  </head>
+
+  <body>
+    <P>
+      <?PHP
+      $product_information = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      print_r($product_information);
+      ?>
+    </P>
+  </body>
+
+</html>
